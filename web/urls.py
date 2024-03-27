98b7f7a8ec55207c8e2ff00fad777df57e5cf48b
Fareed Khalil Sayed <fareedsayed95@gmail.com>
2024-03-28 01:05:49 +0530

    just chat and landing page is remaining

--- web/urls.py
+++ web/urls.py
@@ -2,13 +2,16 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index, student_login_view, mentor_login_view, apply_for_mentor_view, register_view, login_view, logout_view, dashboard_view
+from .views import index, mentor_login_view, apply_for_mentor_view, SignupPage, LoginPage , HomePage, LogoutPage
 
 urlpatterns = [
     path('', index, name='home'),
-    path('student-login/', student_login_view, name='student_login'),
+    path('signup/',SignupPage,name='signup'),
+    path('login/',LoginPage,name='login'),
     path('mentor-login/', mentor_login_view, name='mentor_login'),
     path('apply-for-mentor/', apply_for_mentor_view, name='apply_for_mentor'),
+    path('home/',HomePage,name='home'),
+    path('logout/',LogoutPage,name='logout'),
     
 ]
 
--- web/views.py
+++ web/views.py
@@ -47,11 +47,11 @@ def LoginPage(request):
         user=authenticate(request,username=username,password=pass1)
         if user is not None:
             login(request,user)
-            return redirect('student_login')
+            return redirect('home')
         else:
             return HttpResponse ("Username or Password is incorrect!!!")
 
-    return render (request,'login.html')
+    return render (request,'studentauth/login.html')
 
 def LogoutPage(request):
     logout(request)
